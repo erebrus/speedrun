7f7fb1e218c65bdc5d8c6e6d045b3f20e3d8701a
Cláudio Coelho <claudio.coelho@solenix.ch>
2025-06-17 08:55:19 +0200

    energy pearls

--- godot/src/world/objects/energy_node.gd
+++ godot/src/world/objects/energy_node.gd
@@ -2,7 +2,12 @@ class_name EnergyNode extends Area2D
 
 @export 
 var energy := 10
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+func _ready() -> void:
+	animation_player.play("default")
+	
 func _on_body_entered(body: Node2D) -> void:
 	body.collect(self)
 	destroy()
