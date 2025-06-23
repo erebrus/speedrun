0bf34aeaa016416f50b10b7b0cb5d23a7e96d06c
Ángela <angela.lbsr@gmail.com>
2025-06-23 21:48:04 +0200

    Revert "try fix intro delay"
    This reverts commit 988d38aba375c492b3ca2cbff41fd291a0ef0766.

--- godot/addons/audio_intro/intro_audio_stream_player.gd
+++ godot/addons/audio_intro/intro_audio_stream_player.gd
@@ -4,27 +4,24 @@ class_name IntroAudioStreamPlayer extends AudioStreamPlayer
 
 
 @export var intro_stream: AudioStream
-var _intro_player: AudioStreamPlayer
 var _main_stream: AudioStream = null
 var _playing_intro := false
 
 func play_from_intro():
 	if intro_stream:
-		_intro_player.play()
+		_main_stream = stream
+		stream = intro_stream
 		_playing_intro = true
+		super.play()
 	else:
 		_playing_intro = false
 		super.play(0)
 
 func _ready():
-	_intro_player = AudioStreamPlayer.new()
-	_intro_player.bus = bus
-	_intro_player.stream = intro_stream
-	_intro_player.finished.connect(_on_stream_finished)
-	add_child(_intro_player)
-	
+	connect("finished", _on_stream_finished)
 
 func _on_stream_finished():
-	if _playing_intro:
+	if _playing_intro and _main_stream:
+		stream = _main_stream
 		_playing_intro = false
 		super.play()
