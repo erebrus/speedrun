988d38aba375c492b3ca2cbff41fd291a0ef0766
Ángela <angela.lbsr@gmail.com>
2025-06-23 21:22:07 +0200

    try fix intro delay

--- godot/addons/audio_intro/intro_audio_stream_player.gd
+++ godot/addons/audio_intro/intro_audio_stream_player.gd
@@ -4,24 +4,27 @@ class_name IntroAudioStreamPlayer extends AudioStreamPlayer
 
 
 @export var intro_stream: AudioStream
+var _intro_player: AudioStreamPlayer
 var _main_stream: AudioStream = null
 var _playing_intro := false
 
 func play_from_intro():
 	if intro_stream:
-		_main_stream = stream
-		stream = intro_stream
+		_intro_player.play()
 		_playing_intro = true
-		super.play()
 	else:
 		_playing_intro = false
 		super.play(0)
 
 func _ready():
-	connect("finished", _on_stream_finished)
+	_intro_player = AudioStreamPlayer.new()
+	_intro_player.bus = bus
+	_intro_player.stream = intro_stream
+	_intro_player.finished.connect(_on_stream_finished)
+	add_child(_intro_player)
+	
 
 func _on_stream_finished():
-	if _playing_intro and _main_stream:
-		stream = _main_stream
+	if _playing_intro:
 		_playing_intro = false
 		super.play()
