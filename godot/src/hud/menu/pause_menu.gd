5d4a71386017638043b73befe6bab761e9237750
Ángela <angela.lbsr@gmail.com>
2025-06-23 17:09:08 +0200

    pause menu

---
+++ godot/src/hud/menu/pause_menu.gd
@@ -0,0 +1,50 @@
+extends PanelContainer
+
+
+@export var level_manager: LevelManager
+
+
+func _ready() -> void:
+	%NextLevelButton.visible = not level_manager.is_last_level()
+	
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_released("pause"):
+		if visible:
+			resume()
+		else:
+			pause()
+	
+
+func pause() -> void:
+	get_tree().paused = true
+	show()
+	
+func resume() -> void:
+	hide()
+	get_tree().paused = false
+	
+
+func _on_exit_button_pressed():
+	resume()
+	Globals.go_to_main_menu()
+
+
+func _on_resume_button_pressed():
+	resume()
+
+
+func _on_repeat_level_button_pressed():
+	resume()
+	level_manager.load_current_level()
+
+
+func _on_next_level_button_pressed():
+	resume()
+	level_manager.load_next_level()
+
+func _on_volume_changed(_value: float) -> void:
+	if not is_node_ready():
+		return
+	
+	Globals.ui_sfx.click_sfx.play()
